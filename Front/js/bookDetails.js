4b222e2242459776f216daa816ee9f2fc63fec1e
sergeimlk <sergei.mlk@hotmail.com>
2024-10-25 15:26:51 +0200

    🔍📚 added bookDetails

--- Front/js/bookDetails.js
+++ Front/js/bookDetails.js
@@ -1,9 +1,10 @@
-console.log("hello from bookDetails.js");
-
 const apiKey = "AIzaSyDQrL0-MwiXKvSdzTr6E5KtVcanozHoG90";
+const bookDetails = document.querySelector(".book-detail");
+console.log("object bookDetails", bookDetails);
 
 const queryString_url_id = window.location.search;
 console.log("queryString_url_id", queryString_url_id);
+
 const urlParams = new URLSearchParams(queryString_url_id);
 console.log("urlParams", urlParams);
 const theId = urlParams.get("id");
@@ -27,4 +28,19 @@ getBookDetails();
 function displayBookDetails(datas) {
   console.log(datas);
   console.log(datas.volumeInfo.title);
+  bookDetails.innerHTML = "";
+  const img = document.createElement("img");
+  const title = document.createElement("h1"); 
+  const auteur = document.createElement("h2");
+  const description = document.createElement("p");
+  img.src = datas.volumeInfo.imageLinks.thumbnail;
+  title.innerHTML = datas.volumeInfo.title;
+  auteur.innerHTML = datas.volumeInfo.authors;
+  description.innerHTML = datas.volumeInfo.description;
+  bookDetails.appendChild(img);
+  bookDetails.appendChild(title);
+  bookDetails.appendChild(auteur);
+  bookDetails.appendChild(description);
+
+
 }
