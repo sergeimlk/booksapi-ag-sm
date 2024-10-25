85213331b6fdc20ee6ec969ee2b7b7d2d7c0baf7
Agnès Cappello <senga.ds@gmail.com>
2024-10-25 15:30:56 +0200

    recfif

--- Front/js/bookDetails.js
+++ Front/js/bookDetails.js
@@ -29,31 +29,4 @@ getBookDetails();
 function displayBookDetails(datas) {
   console.log(datas);
   console.log(datas.volumeInfo.title);
-  // bookDetails.innerHTML = "";
-  // const title = document.createElement("h2");
-  // const searchInfo = document.createElement("p");
-  // searchInfo.innerHTML = "Search info: " + datas.searchInfo.textSnippet;
-
-  // title.innerHTML = datas.volumeInfo.title;
-  // bookDetails.appendChild(title);
-  // bookDetails.appendChild(searchInfo);
-  // const img = document.createElement("img");
-  // img.src = datas.volumeInfo.imageLinks.thumbnail;
-  // bookDetails.appendChild(img);
-  // const description = document.createElement("p");
-  // description.innerHTML = datas.volumeInfo.description;
-  // bookDetails.appendChild(description);
-  // const authors = document.createElement("p");
-  // authors.innerHTML = "Authors: " + datas.volumeInfo.authors;
-  // bookDetails.appendChild(authors);
-  // const categories = document.createElement("p");
-  // categories.innerHTML = "Categories: " + datas.volumeInfo.categories;
-  // bookDetails.appendChild(categories);
-  // const pageCount = document.createElement("p");
-  // pageCount.innerHTML = "Page count: " + datas.volumeInfo.pageCount;
-  // bookDetails.appendChild(pageCount);
-  // const language = document.createElement("p");
-  // language.innerHTML = "Language: " + datas.volumeInfo.language;
-  // bookDetails.appendChild(language);
-  // const publishedDate = document.createElement("p");
 }
